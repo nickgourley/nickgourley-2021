2308cd1be6dcad7d92b1b0c58a617507d5f682d7
Nick Gourley <nick.gourley@gmail.com>
2021-01-22 00:56:14 -0500

    fixed unit converter

--- src/components/web-apps/unit-conversion-app.js
+++ src/components/web-apps/unit-conversion-app.js
@@ -2,26 +2,15 @@ import React, { useState, useEffect } from "react";
 import { unitValues } from "../../constants/unitValues";
 
 const UnitConversionApp = () => {
-    const [inputUnits, setInputUnits] = useState('km');
-    const [outputUnits, setOutputUnits] = useState('km');
-    const [input, setInput] = useState('0');
     const [output, setOutput] = useState('0');
-    
+    const [input, setInput] = useState('0');
+    const [inputUnits, setInputUnits] = useState('[km] kilometers');
+    const [outputUnits, setOutputUnits] = useState('[km] kilometers');
     const [unitsType, setUnitsType] = useState('length');
-    
-    const [firstRun, setFirstRun] = useState(true);
+    const [factor, setFactor] = useState(1);
 
-    const handleUnitConversion = () => {
-        if(!isNaN(input)) {
-            const dict = unitValues[unitsType]
-            const factor = dict[outputUnits]/dict[inputUnits];
-            setOutput(input * factor);
-            
-        }
-    }
-
-    const handleUnitsChange = (e) => {
-        const newUnits = e.target.value;
+    const handleUnitsChange = () => {
+        const newUnits = document.getElementById('input-units-type').value;
         const newDefaultUnit = Object.keys(unitValues[newUnits])[0];
         setUnitsType(newUnits);
         setInputUnits(newDefaultUnit);
@@ -29,64 +18,54 @@ const UnitConversionApp = () => {
 
     }
 
-    const handleUnitFormSubmit = (e) => {
-        e.preventDefault();
-        handleUnitConversion();
+
+
+    const calculateOutput = () => {
+        
     }
+    
+    useEffect(() => {
+        setOutput(input * factor);
+    },[input, factor])
+
+    useEffect(() => {
+        const dict = unitValues[unitsType];
+        setFactor(dict[outputUnits]/dict[inputUnits]);
+    }, [inputUnits, outputUnits])
+
 
     const handleClear = (e) => {
         if(e) { e.preventDefault(); }
-        setOutput('0');
-        document.getElementById('input-unit-conversion-app').value = "";
         setInput('0');
     }
 
-    useEffect(() => {
-        console.log('input')
-        handleUnitConversion()
-    }, [inputUnits])
-
-    useEffect(() => {
-        console.log('output')
-        if(!firstRun) {
-            handleUnitConversion()
-        }
-        else {
-            setFirstRun(false);
-            handleClear();
-        }
-    }, [outputUnits])
-
-    
-
     return (
         <>
-            <form onSubmit={handleUnitFormSubmit}>
+            <form>
                 Unit Type:{` `}
-                <select name="unitsType" onChange={handleUnitsChange}>
+                <select id="input-units-type" name="unitsType" onChange={handleUnitsChange}>
                     {Object.entries(unitValues).map(([key, value]) => <option key={key} value={key}>{key}</option>)}
                 </select>
                 <button onClick={handleClear}>Clear</button>
                 <br />
+                <div>Units Type: {unitsType}</div>
+                <div>Input: {input}</div>
+                <div>Output: {output}</div>
+                <div>inputUnits: {inputUnits}</div>
+                <div>outputUnits: {outputUnits}</div>
                 Input:{` `}
                 <input id="input-unit-conversion-app" name="input" onChange={(e) => setInput(e.target.value)} />
                 {` `}
-                {output && output + " " + outputUnits}
+                {output + " " + outputUnits}
                 <br />
                 From:{` `}
-                <select name="unit1" onChange={(e) => {
-                    setInputUnits(e.target.value)
-                }}>
+                <select name="unit1" onChange={(e) => {setInputUnits(e.target.value)}}>
                     {Object.entries(unitValues[unitsType]).map(([key, value]) => <option key={key} value={key}>{key}</option>)}
                 </select>
                 {` `}To:{` `}
-                <select name="unit2" onChange={(e) => { 
-                    setOutputUnits(e.target.value)
-                }}>
+                <select name="unit2" onChange={(e) => setOutputUnits(e.target.value)}>
                     {Object.entries(unitValues[unitsType]).map(([key, value]) => <option key={key} value={key}>{key}</option>)}
                 </select>
-                {` `}
-                <button>Go</button>
                 
             </form>
         </>
